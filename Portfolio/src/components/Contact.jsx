e40361aa6b1812da8c365a1986194599a796b9e1
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-07-22 17:59:34 -0700

    need to add resume and fix styling

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/images/contact-img.svg";
+import 'animate.css';
+// import TrackVisibility from 'react-on-screen';
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
+  const [formDetails, setFormData] = useState({
+    firstName: '',
+    lastname: '',
     email: '',
-    message: ''
+    message: '', 
+    phone: ''
   });
+
+  // const [status, setStatus] = useState({
+  //   message: '',
+  //   success: true
+  // });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -21,23 +32,39 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact">
-      <h2>Contact</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} required />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea id="message" name="message" value={formData.message} onChange={handleInputChange} required></textarea>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <section className="contact" id="contact">
+        <Container> 
+            <Row className="align-items-center">
+                <Col md={6}>
+                    <img src={contactImg} alt="contact-image"/>
+                </Col> 
+                <Col md={6}> 
+                <h2>Get In Touch</h2> 
+                <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col sm={6} className='px-1'>                        
+                      <input 
+                        type="text" 
+                        name="firstName"
+                        value={formDetails.firstName} 
+                        placeholder="First Name" 
+                        onChange={handleInputChange} 
+                        />
+                      </Col>
+                      <Col sm={6} className='px-1'>                        
+                      <input 
+                        type="text" 
+                        name="firstName"
+                        value={formDetails.firstName} 
+                        placeholder="First Name" 
+                        onChange={handleInputChange} 
+                        />
+                      </Col>
+                    </Row>
+                </form>
+                </Col>
+            </Row>
+        </Container>
     </section>
   );
 };
