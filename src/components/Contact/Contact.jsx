0dab1dd3e1bc725ef829b8ef666ed96887f3f4b0
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-11 14:41:22 -0700

    editing contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,42 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import contactImg from "../../assets/images/contact-img.svg";
-import 'animate.css';
-import './Contact.css';
+import "./Contact.css";
 
 const Contact = () => {
   const [formDetails, setFormData] = useState({
     firstName: '',
-    lastname: '',
+    lastName: '',
     email: '',
-    message: '', 
-    phone: ''
+    message: '',
+    phone: '',
   });
 
   const [formErrors, setFormErrors] = useState({});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
       [name]: value
     }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validateForm()) {
-      console.log('Form submitted successfully!', formDetails);
-      setFormData({
-        firstName: '',
-        lastName: '',
-        email: '',
-        message: '',
-        phone: ''
-      });
-    } else {
-      console.log('Form validation failed!', formErrors);
-    }
   };
 
   const validateForm = () => {
@@ -45,95 +28,125 @@ const Contact = () => {
 
     if (!formDetails.firstName.trim()) {
       formIsValid = false;
-      errors.firstName = 'Please enter your first name';
+      errors.firstName = "Please enter your first name";
     }
 
     if (!formDetails.lastName.trim()) {
       formIsValid = false;
-      errors.lastName = 'Please enter your last name';
-    } 
+      errors.lastName = "Please enter your last name";
+    }
 
     const emailRegex = /\S+@\S+\.\S+/;
     if (!formDetails.email.trim()) {
       formIsValid = false;
-      errors.email = 'Please enter your email';
+      errors.email = "Please enter your email";
     } else if (!emailRegex.test(formDetails.email)) {
       formIsValid = false;
-      errors.email = 'Please enter a valid email address';
+      errors.email = "Please enter a valid email address";
     }
-    
+
     setFormErrors(errors);
     return formIsValid;
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Optionally handle success or error responses
+      console.log("Form submitted successfully!", formDetails);
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+        phone: '',
+      });
+    } else {
+      console.log("Form validation failed!", formErrors);
+    }
+  };
+
   return (
     <section className="contact" id="contact">
-        <Container> 
-            <Row className="align-items-center">
-                <Col md={6}>
-                    <img src={contactImg} alt="contact-image"/>
-                </Col> 
-                <Col md={6}> 
-                <h2>Get In Touch</h2> 
-                <form onSubmit={handleSubmit}>
-                    <Row>
-                      <Col sm={6} className='px-1'>                        
-                      <input 
-                        type="text" 
-                        name="firstName"
-                        value={formDetails.firstName} 
-                        placeholder="First Name" 
-                        onChange={handleInputChange} 
-                        className={formErrors.firstName && 'is-invalid'}
-                        />
-                        {formErrors.firstName && <span className="error">{formErrors.firstName}</span>}
-                      </Col>
-                      <Col sm={6} className='px-1'>                        
-                      <input 
-                        type="text" 
-                        name="lastName"
-                        value={formDetails.lastName} 
-                        placeholder="Last Name" 
-                        onChange={handleInputChange} 
-                        className={formErrors.firstName && 'is-invalid'}
-                        />
-                        {formErrors.lastName && <span className="error">{formErrors.lastName}</span>}
-                        </Col>
-                      <Col sm={6} className='px-1'>                        
-                      <input 
-                        type="text" 
-                        name="email"
-                        value={formDetails.email} 
-                        placeholder="Email Address" 
-                        onChange={handleInputChange} 
-                        className={formErrors.email && 'is-invalid'}
-                        />
-                        {formErrors.email && <span className="error">{formErrors.email}</span>}
-                      </Col>
-                      <Col sm={6} className='px-1'>                        
-                      <input 
-                        type="text" 
-                        name="phone"
-                        value={formDetails.phone} 
-                        placeholder="Phone Number" 
-                        onChange={handleInputChange} 
-                        />
-                      </Col>
-                      <Col sm={12} className='px-1'>                        
-                      <input 
-                        type="text" 
-                        name="message"
-                        value={formDetails.message} 
-                        placeholder="Message" 
-                        onChange={handleInputChange} 
-                       />
-                      </Col>
-                    </Row>
-                    <Button type="submit">Submit</Button>
-                </form>
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="contact-image" />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form
+              onSubmit={handleSubmit}
+              data-netlify="true"
+              name="contact"
+              method="post"
+              data-netlify-honeypot="bot-field" // adds honeypot field for spam protection
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formDetails.firstName}
+                    placeholder="First Name"
+                    onChange={handleInputChange}
+                    className={formErrors.firstName ? "is-invalid" : ""}
+                  />
+                  {formErrors.firstName && (
+                    <span className="error">{formErrors.firstName}</span>
+                  )}
                 </Col>
-            </Row>
-        </Container>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formDetails.lastName}
+                    placeholder="Last Name"
+                    onChange={handleInputChange}
+                    className={formErrors.lastName ? "is-invalid" : ""}
+                  />
+                  {formErrors.lastName && (
+                    <span className="error">{formErrors.lastName}</span>
+                  )}
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="email"
+                    name="email"
+                    value={formDetails.email}
+                    placeholder="Email Address"
+                    onChange={handleInputChange}
+                    className={formErrors.email ? "is-invalid" : ""}
+                  />
+                  {formErrors.email && (
+                    <span className="error">{formErrors.email}</span>
+                  )}
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    name="phone"
+                    value={formDetails.phone}
+                    placeholder="Phone Number"
+                    onChange={handleInputChange}
+                  />
+                </Col>
+                <Col sm={12} className="px-1">
+                  <textarea
+                    name="message"
+                    value={formDetails.message}
+                    placeholder="Message"
+                    onChange={handleInputChange}
+                    rows="4"
+                  />
+                </Col>
+              </Row>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
